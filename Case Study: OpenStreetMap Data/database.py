9437aed97fcce43618f1f2ff2e13bf22e6572d5b
stephenisau <sfcstau@gmail.com>
2020-06-22 19:45:39 -0700

    seed postgres database

--- Case Study: OpenStreetMap Data/database.py
+++ Case Study: OpenStreetMap Data/database.py
@@ -1,59 +1,58 @@
 import csv
-import sqlite3
+import psycopg2
 
-con = sqlite3.connect("san_francisco.db")
-con.text_factory = str
-cur = con.cursor()
+conn = psycopg2.connect("host=127.0.0.1 san_francisco.db")
+cur = conn.cursor()
 
 # create nodes table
 cur.execute(
     "CREATE TABLE nodes (id, lat, lon, user, uid, version, changeset, timestamp);")
-with open('nodes.csv', 'rb') as fin:
-    dr = csv.DictReader(fin)
+with open('nodes.csv', 'rb') as f:
+    d = csv.DictReader(f)
     to_db = [(i['id'], i['lat'], i['lon'], i['user'], i['uid'], i['version'], i['changeset'], i['timestamp'])
-             for i in dr]
+             for i in d]
 
 cur.executemany("INSERT INTO nodes (id, lat, lon, user, uid, version, changeset, timestamp) \
                 VALUES (?, ?, ?, ?, ?, ?, ?, ?);", to_db)
-con.commit()
+conn.commit()
 
 # create nodes_tags table
 cur.execute("CREATE TABLE nodes_tags (id, key, value, type);")
-with open('nodes_tags.csv', 'rb') as fin:
-    dr = csv.DictReader(fin)
-    to_db = [(i['id'], i['key'], i['value'], i['type']) for i in dr]
+with open('nodes_tags.csv', 'rb') as f:
+    dd = csv.DictReader(f)
+    to_db = [(i['id'], i['key'], i['value'], i['type']) for i in d]
 
 cur.executemany(
     "INSERT INTO nodes_tags (id, key, value, type) VALUES (?, ?, ?, ?);", to_db)
-con.commit()
+conn.commit()
 
 # Create ways table
 cur.execute("CREATE TABLE ways (id, user, uid, version, changeset, timestamp);")
-with open('ways.csv', 'rb') as fin:
-    dr = csv.DictReader(fin)
+with open('ways.csv', 'rb') as f:
+    d = csv.DictReader(f)
     to_db = [(i['id'], i['user'], i['uid'], i['version'],
-              i['changeset'], i['timestamp']) for i in dr]
+              i['changeset'], i['timestamp']) for i in d]
 
 cur.executemany(
     "INSERT INTO ways (id, user, uid, version, changeset, timestamp) VALUES (?, ?, ?, ?, ?, ?);", to_db)
-con.commit()
+conn.commit()
 
 # Create ways_nodes table
 cur.execute("CREATE TABLE ways_nodes (id, node_id, position);")
-with open('ways_nodes.csv', 'rb') as fin:
-    dr = csv.DictReader(fin)
-    to_db = [(i['id'], i['node_id'], i['position']) for i in dr]
+with open('ways_nodes.csv', 'rb') as f:
+    d = csv.DictReader(f)
+    to_db = [(i['id'], i['node_id'], i['position']) for i in d]
 
 cur.executemany(
     "INSERT INTO ways_nodes (id, node_id, position) VALUES (?, ?, ?);", to_db)
-con.commit()
+conn.commit()
 
 # Create ways_tags table
 cur.execute("CREATE TABLE ways_tags (id, key, value, type);")
-with open('ways_tags.csv', 'rb') as fin:
-    dr = csv.DictReader(fin)
-    to_db = [(i['id'], i['key'], i['value'], i['type']) for i in dr]
+with open('ways_tags.csv', 'rb') as f:
+    d = csv.DictReader(f)
+    to_db = [(i['id'], i['key'], i['value'], i['type']) for i in d]
 
 cur.executemany(
     "INSERT INTO ways_tags (id, key, value, type) VALUES (?, ?, ?, ?);", to_db)
-con.commit()
+conn.commit()
---
+++ README.md
@@ -0,0 +1,8 @@
+# Data wrangling techniques utilizing Python
+- Course can be found here: (https://www.udacity.com/course/data-wrangling-with-mongodb--ud032)
+
+# References
+
+- https://sriramjaju.github.io/2017-06-16-openstreetmap-data-wrangling-with-sql/
+- http://luizschiller.com/openstreetmap/
+- https://arjan-hada.github.io/osm-data-wrangling.html
